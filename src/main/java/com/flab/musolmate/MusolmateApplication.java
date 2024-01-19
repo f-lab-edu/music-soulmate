3196fe5c94e9674c02fbe426e1651f8bfa340e33
grey <charminggw@gmail.com>
2024-01-19 17:24:45 +0900

    feat(all): initialize a project

---
+++ src/main/java/com/flab/musolmate/MusolmateApplication.java
@@ -0,0 +1,13 @@
+package com.flab.musolmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusolmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusolmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/musolmate/MusolmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.musolmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusolmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
