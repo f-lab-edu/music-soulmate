44657890416c6aef9b83de1bc376c15d2fbd7601
grey <charminggw@gmail.com>
2024-01-24 19:52:52 +0900

    test(member): ControllerTest 내 불필요한 검증 제거

--- src/test/java/com/flab/musolmate/member/web/MemberApiControllerTest.java
+++ src/test/java/com/flab/musolmate/member/web/MemberApiControllerTest.java
@@ -80,8 +80,6 @@ public class MemberApiControllerTest {
 
         // then
         assertThat( responseEntity.getStatusCode() ).isEqualTo( HttpStatus.CREATED );
-        assertThat( responseEntity.getBody() ).isNotNull();
-
         List< Member > all = memberRepository.findAll();
         assertThat( all.get( 0 ).getEmail() ).isEqualTo( email );
         assertThat( passwordEncoder.matches( password, all.get( 0 ).getPassword() ) ).isTrue();
