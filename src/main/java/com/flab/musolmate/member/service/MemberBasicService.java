cd3c93afde258d6cc447299fff1090bad20d9d10
grey <charminggw@gmail.com>
2024-01-24 20:38:45 +0900

    refactor(member): 이메일, 닉네임 중복 검사시 주석 대신 메소드 추출

--- src/main/java/com/flab/musolmate/member/service/MemberBasicService.java
+++ src/main/java/com/flab/musolmate/member/service/MemberBasicService.java
@@ -24,16 +24,18 @@ public class MemberBasicService {
     @Transactional
     public Member registerMember( MemberRegisterRequest requestDto ) {
         /* 클라이언트에서 하겠지만 서버에서 한번 더 체크 */
-        // 이메일 중복 검사
+        checkDuplicateEmailAndNickName( requestDto );
+
+        return memberRepository.save( requestDto.toEntity( passwordEncoder.encode( requestDto.getPassword() ) ) );
+    }
+
+    private void checkDuplicateEmailAndNickName( MemberRegisterRequest requestDto ) {
+
         if ( memberRepository.existsByEmail( requestDto.getEmail() ) ) {
             throw new DuplicateMemberException( "이미 존재하는 이메일입니다." );
         }
-        // 닉네임 중복 검사
         if ( memberRepository.existsByNickName( requestDto.getNickName() ) ) {
             throw new DuplicateMemberException( "이미 존재하는 닉네임입니다." );
         }
-
-        // 비밀번호 암호화 + save
-        return memberRepository.save( requestDto.toEntity( passwordEncoder.encode( requestDto.getPassword() ) ) );
     }
 }
