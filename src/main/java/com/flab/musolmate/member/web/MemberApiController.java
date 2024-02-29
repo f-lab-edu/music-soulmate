974f48d663d67d183bbce8228d649d3453bb4315
grey <charminggw@gmail.com>
2024-02-29 19:03:09 +0900

    feat(response): API 성공시 응답값에 ApiResponse 적용

--- src/main/java/com/flab/musolmate/member/web/MemberApiController.java
+++ src/main/java/com/flab/musolmate/member/web/MemberApiController.java
@@ -1,5 +1,6 @@
 package com.flab.musolmate.member.web;
 
+import com.flab.musolmate.common.domain.api.ApiResponse;
 import com.flab.musolmate.member.domain.entity.Member;
 import com.flab.musolmate.member.service.MemberBasicService;
 import com.flab.musolmate.member.web.request.MemberRegisterRequest;
@@ -12,35 +13,37 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/members")
+@RequestMapping( "/members" )
 public class MemberApiController {
     private final MemberBasicService memberBasicService;
 
     /**
      * 회원가입
+     *
      * @param requestDto
      * @return
      */
-    @PostMapping("/register")
-    public ResponseEntity<Member> registerMember( @Valid @RequestBody MemberRegisterRequest requestDto) {
+    @PostMapping( "/register" )
+    public ResponseEntity< ApiResponse< Member > > registerMember( @Valid @RequestBody MemberRegisterRequest requestDto ) {
 
         Member registeredMember = memberBasicService.registerMember( requestDto );
-        return new ResponseEntity<>( registeredMember, HttpStatus.CREATED );
+        return ResponseEntity.status( HttpStatus.CREATED ).body( ApiResponse.createSuccess( registeredMember ) );
     }
 
-    @GetMapping("/{id}")
+    @GetMapping( "/{id}" )
     @PreAuthorize( "hasAnyAuthority('ROLE_ADMIN')" )
-    public ResponseEntity<Member> getMember( @PathVariable Long id ) {
+    public ResponseEntity< ApiResponse< Member > > getMember( @PathVariable Long id ) {
         Member member = memberBasicService.getMemberWithAuthorities( id ).orElse( null );
         if ( member == null ) {
             return new ResponseEntity<>( HttpStatus.NOT_FOUND ); // TODO. 커스텀 예외 처리
         }
-        return ResponseEntity.ok( member );
+        return ResponseEntity.status( HttpStatus.OK ).body( ApiResponse.createSuccess( member ) );
     }
 
-    @GetMapping("/me")
+    @GetMapping( "/me" )
     @PreAuthorize( "hasAnyAuthority('ROLE_USER')" )
-    public ResponseEntity<Member> getMyMember() {
-        return ResponseEntity.ok( memberBasicService.getMyMemberWithAthorities().get() );
+    public ResponseEntity< ApiResponse< Member > > getMyMember() {
+        Member member = memberBasicService.getMyMemberWithAthorities().orElse( null );
+        return ResponseEntity.status( HttpStatus.OK ).body( ApiResponse.createSuccess( member ) );
     }
 }
