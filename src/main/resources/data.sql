f08ff83f4572f3636df3f08b73f7a48069c1dd48
grey <charminggw@gmail.com>
2024-03-21 23:01:53 +0900

    fix(db): dev용 DB 연결을 위해 mariaDB Driver 추가 및 로컬 properties 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into MEMBERS ( EMAIL, PASSWORD, NICK_NAME) values ('admin@gmail.com', '$2a$10$F2JsXoyV3Ws9o6m1wmGaC.d5QT9pK6rNLpgwbCEQH1JuXm4HD6On6', 'admin');
+insert into members ( EMAIL, PASSWORD, NICK_NAME) values ('admin@gmail.com', '$2a$10$F2JsXoyV3Ws9o6m1wmGaC.d5QT9pK6rNLpgwbCEQH1JuXm4HD6On6', 'admin');
 
-insert into AUTHORITY (AUTHORITY_NAME) values ('ROLE_USER');
-insert into AUTHORITY (AUTHORITY_NAME) values ('ROLE_ADMIN');
+insert into authority (AUTHORITY_NAME) values ('ROLE_USER');
+insert into authority (AUTHORITY_NAME) values ('ROLE_ADMIN');
 
-insert into MEMBER_AUTHORITY (MEMBER_ID, AUTHORITY_NAME) values (1, 'ROLE_USER');
-insert into MEMBER_AUTHORITY (MEMBER_ID, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
+insert into member_authority (MEMBER_ID, AUTHORITY_NAME) values (1, 'ROLE_USER');
+insert into member_authority (MEMBER_ID, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
